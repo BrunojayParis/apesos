53dc1eee09b9f9f166774cb7cf4a3365290f60ca
Bruno <brunojparisz@gmail.com>
2023-01-28 16:28:48 -0300

    adding github workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App', () => {
+  const component = render(<App />);
+
+  component.getByText('Salario en pesos')
+
 });
+
